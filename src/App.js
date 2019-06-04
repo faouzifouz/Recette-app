219df47cbdcebffa306687219fbe16c5f432d9ef
Titah Mohamed Faouzi <46715773+faouzifouz@users.noreply.github.com>
2019-06-04 11:53:45 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+// CSS
+import './App.css'
+import Header from './components/Header'
+import recettes from './recettes'
+import Admin from './components/Admin';
+import Card from './components/Card'
+
+class App extends Component {
+  state = {
+    pseudo: this.props.match.params.pseudo,
+    recettes: {}
+  }
+
+  chargerExemple = () => this.setState({ recettes })
+
+  render () {
+    const cards = Object.keys(this.state.recettes)
+    .map(key => <Card key={key} details={this.state.recettes[key]} />)
+    
+    return (
+      <div className='box'>
+        <Header pseudo={this.state.pseudo} />
+        <div className='cards'>
+          { cards }
+        </div>
+        <Admin
+          chargerExemple={this.chargerExemple} />
+      </div>
+    )
+  }
+}
+
+export default App
