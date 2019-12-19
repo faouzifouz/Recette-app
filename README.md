165a6d419de62591fd30a18924bb45547943f832
Titah Mohamed Faouzi <46715773+faouzifouz@users.noreply.github.com>
2019-12-19 11:26:28 +0100

    fin de l'appli

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Recette-app
-Application de recette en react
+# Recette-app (React)
+Application de  Blog de  recette en react
+
+## Fonctionnalité de l'appli
+
+* Create
+* Read
+* Update
+* Delete
+
+### +
+
+* Base de données (Firebase)
+* Authentification (Facebook)
+
+
+
+
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react';
 import AjouterRecette from './AjouterRecette';
 import AdminForm from './AdminForm';
+import Login from './Login';
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import base, { firebaseApp } from '../base'
+
 
 class Admin extends Component {
+  state = {
+    uid: null,
+    chef: null
+  }
+
+  componentDidMount () {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user){
+        this.handleAuth({user})
+      }
+    })
+  }
+
+  handleAuth = async authData => {
+    const box = await base.fetch(this.props.pseudo, {contex: this})
+    if(!box.chef) {
+      await base.post(`${this.props.pseudo}/chef`, {
+        data:authData.user.uid
+      })
+    }
+    this.setState({
+      uid: authData.user.uid,
+      chef: box.chef || authData.user.uid
+    })
+  }
+
+  authenticate = () => {
+    const authProvider = new firebase.auth.FacebookAuthProvider()
+    firebaseApp
+    .auth()
+    .signInWithPopup(authProvider)
+    .then(this.handleAuth)
+  }
+
+  logout = async () => {
+    console.log('Déconnextion')
+    await firebase.auth().signOut()
+    this.setState({ uid:null })
+  }
+
   render() {
     const { recettes, ajouterRecette,majRecette, 
       chargerExemple, supprimerRecette  } = 
     this.props
+
+    const logout = <button onClick={this.logout}>Déconnection</button>
+
+    //Si l'utilisateur n'est pas connecté
+      if (!this.state.uid) {
+        return <Login authenticate={this.authenticate} />
+      }
+
+      if(this.state.uid !== this.state.chef){
+        return (
+          <div>
+            <p>Tu n'est pas le chef de cette boîte !</p>
+            {logout}
+          </div>
+        )
+      }
+
     return (
       <div className="cards">
         <AjouterRecette ajouterRecette={ajouterRecette} />
@@ -21,6 +84,7 @@ class Admin extends Component {
               </AdminForm>)
         }
       <footer> 
+        {logout}
           <button onClick={chargerExemple}>Remplir</button>
        </footer>
       </div>
